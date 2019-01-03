03616cab3a47b1e32daaad93f181e6005d468b91
jaylamrin <jaylamrin@gmail.com>
2019-01-03 12:11:48 -0700

    Finally. All tests working

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,34 @@
 import requests
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    api_url = 'https://api.github.com/users/{u}/gists'.format(u=username)
+    api_response = requests.get(api_url)
+    if api_response.status_code != requests.codes.ok:
+        api_response.raise_for_status()
+        return
+
+    query = """
+    INSERT into gists('github_id', 'html_url', 'git_pull_url', 'git_push_url', 'commits_url', 'forks_url','public', 
+            'created_at', 'updated_at', 'comments', 'comments_url')
+            VALUES
+            (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, :public,
+            :created_at, :updated_at, :comments, :comments_url)
+    """
+    for gist in api_response.json():
+        params = {
+            'github_id': gist['id'],
+            'html_url': gist['html_url'],
+            'git_pull_url': gist['git_pull_url'],
+            'git_push_url': gist['git_push_url'],
+            'commits_url': gist['commits_url'],
+            'forks_url': gist['forks_url'],
+            'public': gist['public'],
+            'created_at': gist['created_at'],
+            'updated_at': gist['updated_at'],
+            'comments': gist['comments'],
+            'comments_url': gist['comments_url']
+        }
+        db.execute(query,params)
+    if commit:
+        db.commit()
+    return
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,36 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    sql = list()
+    sql.append('SELECT * FROM gists')
+    if kwargs:
+        sql.append(' WHERE ')
+        if 'github_id' in kwargs.keys():
+            sql.append(''.join("github_id = '%s'" % v for k, v in kwargs.items() if k == 'github_id'))
+            if len(kwargs.keys()) > 1:
+                sql.append(' AND ')
+        if 'created_at' in kwargs.keys():
+            sql.append(''.join("datetime(created_at) = '%s'" % v for k, v in kwargs.items() if k == 'created_at'))
+            if len(kwargs.keys()) > 1:
+                sql.append(' AND ')
+        if 'created_at__gte' in kwargs.keys():
+            sql.append(''.join("datetime(created_at) >= '%s'" % v for k, v in kwargs.items() if k == 'created_at__gte'))
+            if len(kwargs.keys()) > 1:
+                sql.append(' AND ')
+        if 'created_at__lt' in kwargs.keys():
+            sql.append(''.join("datetime(created_at) < '%s'" % v for k, v in kwargs.items() if k == 'created_at__lt'))
+            if len(kwargs.keys()) > 1:
+                sql.append(' AND ')
+        if 'created_at__lte' in kwargs.keys():
+            sql.append(''.join("datetime(created_at) <= '%s'" % v for k, v in kwargs.items() if k == 'created_at__lte'))
+            if len(kwargs.keys()) > 1 and 'updated_at__gte' in kwargs.keys():
+                sql.append(' AND ')
+        if 'updated_at__gte' in kwargs.keys():
+            sql.append(''.join("datetime(updated_at) >= '%s'" % v for k, v in kwargs.items() if k == 'updated_at__gte'))
+    sql.append(';')
+    query = ''.join(sql)
+    c = db_connection.cursor()
+    c.execute(query)
+    result = [Gist(row) for row in c.fetchall()]
+    return result
+
